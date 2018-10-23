6830fb9b98523c552182b66a27bc987568c0ed41
StatsThoughts <bryant.yang98@gmail.com>
2018-10-23 14:57:10 +0100

    Just moved the function from optimised_lmBoot which was not an R script for some reason?

--- code/lmBoot.r
+++ code/lmBoot.r
@@ -1,27 +1,26 @@
-lmBoot <- function(inputData, nBoot){
-  
-  for(i in 1:nBoot){
-    
-    # resample our data with replacement
-    bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
-    
-    # fit the model under this alternative reality
-    bootLM <- lm(y ~ x, data = bootData)
-    
-    # store the coefs
-    if(i == 1){
-      
-      bootResults <- matrix(coef(bootLM), ncol = 2)
+library(parallel)
 
-      } else {
-      
-      bootResults<- rbind(bootResults, matrix(coef(bootLM), ncol = 2))
+#For higher efficiency split calculations between cores
+nCores <- detectCores()
+myClust <- makeCluster(nCores-1, type = "PSOCK") 
 
-    }
-    
-
-  } # end of i loop
+lmBoot <- function(inputData, nBoot) {
   
-  bootResults
+  #Create matrix to store results
+  bootResults <- matrix(nrow = nBoot, ncol = (ncol(inputData)))
   
+  inputLM <- lm(inputData)
+  bootResults[1,] <- coef(inputLM)
+  
+  bootResults[2:nBoot,1:ncol(inputData)] <- parSapply(myClust, 2:nBoot, function(i) {
+    #resample data with replacement and fit model to this resample
+    resample <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = TRUE),]
+    bootLM <- lm(data = resample)
+    return(t(coef(bootLM)))})
+  
+  #Set column names of matrix to the estimated parameters
+  colnames(bootResults) <- names(coef(inputLM))
+  return(bootResults)
 }
+
+

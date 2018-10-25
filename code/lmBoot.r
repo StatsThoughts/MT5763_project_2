04fd0a5676d4b3638043974d6941b7eba6cb5d3d
Euan Enticott <euanenticott@gmail.com>
2018-10-25 13:18:15 +0100

    Fixed lmBoot and added capability to choose response variable for the lm models created

--- code/lmBoot.r
+++ code/lmBoot.r
@@ -1,24 +1,37 @@
 library(parallel)
 # This line just in case anyone forgets to import data 
-fitness <- read_csv("data/fitness.csv")
+fitness <- read.csv("data/fitness.csv")
 
 #For higher efficiency split calculations between cores
 nCores <- detectCores()
 myClust <- makeCluster(nCores-1, type = "PSOCK") 
 
-lmBoot <- function(inputData, nBoot) {
+
+lmBoot <- function(inputData, nBoot, response) {
+  #Inputs: 
+  #inputData - The data that you wish to boostrap on
+  #nBoot - The number of bootstraps to use
+  #response - The response variable that you are interested in
+  #NOTE: Will fit a model of response against all other columns in the 
+  #      inputted data frame, if you wish to fit more specific models
+  #      then input a subsetted data frame
   
+  #reorder data with response variable first
+  #Do this as lm command will take first column as no response with no other
+  #args given
+  inputData <- inputData[,c(which(colnames(inputData)==response),which(colnames(inputData)!=response))]
   #Create matrix to store results
   bootResults <- matrix(nrow = nBoot, ncol = (ncol(inputData)))
   
   inputLM <- lm(inputData)
   bootResults[1,] <- coef(inputLM)
   
-  bootResults[2:nBoot,1:ncol(inputData)] <- parSapply(myClust, 2:nBoot, function(i) {
+  clusterExport(myClust, "inputData")
+  bootResults[2:nBoot,] <- t(parSapply(myClust, 2:nBoot, function(i) {
     #resample data with replacement and fit model to this resample
     resample <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = TRUE),]
     bootLM <- lm(data = resample)
-    return(t(coef(bootLM)))})
+    return((coef(bootLM)))}))
   
   #Set column names of matrix to the estimated parameters
   colnames(bootResults) <- names(coef(inputLM))

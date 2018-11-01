9b66aa33695a2a47c82440bb883799dd2120e8c8
shuang1234 <43880373+shuang1234@users.noreply.github.com>
2018-11-01 18:44:13 +0000

    Add code for R plots

--- code/PlotCode.R
+++ code/PlotCode.R
@@ -7,3 +7,10 @@ times$State <- factor(times$State, levels = times$State)
 ggplot(data = times, aes(x=State, y = Time, fill = State)) + geom_bar(stat="identity") + guides(fill=FALSE)
 
 
+# Plotting code for R timings -------------------------------------------
+system.time(lmBoot(fitness,1000,"Oxygen", myClust))# 1.24s
+system.time(lmBootOld(fitness,1000))# 2.00s
+
+timesR <- data.frame(State = c("Original version", "Optimized Version"), Time = c(2.00,1.24))
+timesR$State <- factor(timesR$State, levels = timesR$State)
+ggplot(data = timesR, aes(x=State, y = Time, fill = State)) + geom_bar(stat="identity") + guides(fill=FALSE)

4e9b6c2368db1b8005b32e8c107b862e3fce2563
Charisse <diamond_la@163.com>
2018-11-03 17:52:04 +0000

    changed code and made plot

--- code/Example analysis.R
+++ code/Example analysis.R
@@ -1,10 +1,14 @@
 #-------------------------------------- example analysis ------------------------------------------#
 library(tidyverse)
-plot(trees, col = 'purple')
+# plot original data to find out if they have obviously relationship
+plot(trees, col = 'blue')
 # dataMean <- c(mean(trees[,1]), mean(trees[,2]), mean(trees[,3]))
 
+# run our bootstrap
+# input data set is the trees, run for 1000 times, reponse variable is Height 
 testResult <- lmBoot(trees, 1000, response = "Height")
 plot(testResult)
+
 hist(testResult[,1], col = "slateblue4", main = 'intercept distribution')
 hist(testResult[,2], col = "slateblue4", main = 'slope distribution')
 hist(testResult[,3], col = "slateblue4", main = 'slope distribution')
@@ -15,14 +19,11 @@ inputCoef <- testResult[1, ]
 # mean coef number for resampled data
 testMean <- c(mean(testResult[2:1000,1]), mean(testResult[2:1000,2]), mean(testResult[2:1000,3]))
 
-# confidence interval
+# confidence interval by our bootstrap 
 rbind(quantile(testResult[2:1000,1], probs = c(0.025, 0.975)),
       quantile(testResult[2:1000,2], probs = c(0.025, 0.975)),
       quantile(testResult[2:1000,3], probs = c(0.025, 0.975)))
 
-plot(testResult[2:1000,1])
-plot(testResult[2:1000,2])
-plot(testResult[2:1000,3])
-
+# write.csv(trees, "trees.csv")
 
  

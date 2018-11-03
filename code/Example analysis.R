5fd393f58d08e1d6746fe8dfee543cd004998d45
Charisse <diamond_la@163.com>
2018-11-03 21:41:02 +0000

    changed code and plots

--- code/Example analysis.R
+++ code/Example analysis.R
@@ -1,15 +1,14 @@
 #-------------------------------------- example analysis ------------------------------------------#
 library(tidyverse)
-library(ggplot2)
 # plot original data to find out if they have obviously relationship
 plot(trees, col = 'blue')
 # dataMean <- c(mean(trees[,1]), mean(trees[,2]), mean(trees[,3]))
 
 # run our bootstrap
 # input data set is the trees, run for 1000 times, reponse variable is Height 
+set.seed(2345)
 testResult <- lmBoot(trees, 1000, response = "Height")
 # plot the result 
-par(mfrow = c(1,2))
 boxplot(testResult[2:1000, 1], col = "darkgreen")
 boxplot(testResult[2:1000, 2:3], col = "darkgreen")
 

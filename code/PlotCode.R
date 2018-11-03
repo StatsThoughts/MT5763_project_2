91a7fb1c6a4fbdb2d9d4a7237b1440630fbd40bf
Euan Enticott <euanenticott@gmail.com>
2018-11-03 22:50:17 +0000

    Changed plotcode to add another version and added a title to graph

--- code/PlotCode.R
+++ code/PlotCode.R
@@ -44,6 +44,43 @@ library(boot)
 load(file = "RPlotFunctions.RData")
 
 
+#-------------------------CODE FOR PREVIOUS FUnCTION WITH NO PARALLISATION
+
+lmBootNoPara <- function(inputData, nBoot, response = NA, myClust) {
+  #Inputs: 
+  #inputData - The data that you wish to boostrap on
+  #nBoot - The number of bootstraps to use
+  #response - The response variable that you are interested in. No input means first column
+  #clusterType - Passes to type of cluster n makeCluster 
+  #NOTE: Will fit a model of response against all other columns in the 
+  #      inputted data frame, if you wish to fit more specific models
+  #      then input a subsetted data frame
+  
+  # Defaults to first column if no response is given 
+  if(is.na(response)){response<-colnames(inputData)[1]}
+  
+  #reorder data with response variable first
+  #Do this as lm command will take first column as no response with no other
+  #args given
+  inputData <- inputData[,c(which(colnames(inputData)==response),which(colnames(inputData)!=response))]
+  #Create matrix to store results
+  bootResults <- matrix(nrow = nBoot, ncol = (ncol(inputData)))
+  
+  inputLM <- lm(inputData)
+  bootResults[1,] <- coef(inputLM)
+  
+  bootResults[2:nBoot,] <- t(sapply(2:nBoot, function(i) {
+    #resample data with replacement and fit model to this resample
+    resample <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = TRUE),]
+    bootLM <- lm(data = resample)
+    return((coef(bootLM)))}))
+  
+  #Set column names of matrix to the estimated parameters
+  colnames(bootResults) <- names(coef(inputLM))
+  
+  return(bootResults)
+}
+
 #-------------------------CODE FOR THE PREVIOUS FUNCTION ITERATION WITH INTERNALLY DEFINED CLUSTER----------------------------------
 
 
@@ -112,9 +149,16 @@ Benchmark1<- microbenchmark(
   times = 100
 )  
 
-# Benchmark Code for first iteration (internally defined clusters)
+# Benchmark code for second version (Using sapply)
 set.seed(1234)
 Benchmark2<- microbenchmark(
+  lmBootNoPara(FitnessTwoVars,1000, "Oxygen"),
+  times = 100
+)  
+
+# Benchmark Code for third version (internally defined clusters)
+set.seed(1234)
+Benchmark3<- microbenchmark(
   lmBootInCluster(FitnessTwoVars,1000,"Oxygen"),
   times = 100
 )  
@@ -124,7 +168,7 @@ Benchmark2<- microbenchmark(
 nCores <- detectCores()
 myClust <- makeCluster(nCores-1, type = "PSOCK")
 set.seed(1234)
-Benchmark3<- microbenchmark(
+Benchmark4<- microbenchmark(
   lmBoot(FitnessTwoVars,1000, response = "Oxygen", myClust = myClust),
   times = 100
 )  
@@ -134,7 +178,7 @@ stopCluster(myClust)
 
 # Benchmark code for boot package function. 
 set.seed(1234)
-Benchmark4<- microbenchmark(
+Benchmark5<- microbenchmark(
   boot(FitnessTwoVars, BootStatistic, R = 1000, responseCol = 2, parallel = "multicore", ncpus= nCores-1),
   times = 100
 )  
@@ -145,5 +189,7 @@ Benchmark4<- microbenchmark(
 #-------------------------------------- BENCHMARK PLOTS SHOWWING INCREASE IN SPEED ACROSS FUNCTION ITERATIONS------------------------
 
 
-BenchmarkPlot <- rbind(Benchmark1, Benchmark2, Benchmark3, Benchmark4)
-boxplot(BenchmarkPlot, unit="ms", log = F, ylab = "Time (milliseconds)", xlab = "Function", names = c("Original", "First iteration", "Final iteration", "boot"))
+BenchmarkPlot <- rbind(Benchmark1, Benchmark2, Benchmark3, Benchmark4, Benchmark5)
+boxplot(BenchmarkPlot, unit="ms", log = F, ylab = "Time (milliseconds)", xlab = "Function", 
+        names = c("Original", "Second version", "Third version","Final version", "boot"),
+        main = "lmBoot timings comparison")

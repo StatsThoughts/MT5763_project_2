8c7ede06e60d75f31656cc4194c14f72b11e47c2
StatsThoughts <bryant.yang98@gmail.com>
2018-11-04 23:00:11 +0000

    Changes to Sample analysis
    Modified code to reflect outputs of the analysis

--- code/Example analysis.R
+++ code/Example analysis.R
@@ -7,26 +7,19 @@ plot(trees, col = 'blue')
 # run our bootstrap
 # input data set is the trees, run for 1000 times, reponse variable is Height 
 set.seed(2345)
+source("code/lmBoot.R")
 testResult <- lmBoot(trees, 1000, response = "Height")
-# plot the result 
-boxplot(testResult[2:1000, 1], col = "darkgreen")
-boxplot(testResult[2:1000, 2:3], col = "darkgreen")
 
 hist(testResult[2:1000, 1], col = "slateblue4", main = 'intercept distribution')
 hist(testResult[2:1000, 2], col = "slateblue4", main = 'Grith slope distribution')
 hist(testResult[2:1000, 3], col = "slateblue4", main = 'Volume slope distribution')
 
-# coef number for input data
-inputCoef <- testResult[1, ]
-
-# mean coef number for resampled data
-testMean <- c(mean(testResult[2:1000, 1]), mean(testResult[2:1000,2]), mean(testResult[2:1000,3]))
 
 # confidence interval by our bootstrap 
 rbind(quantile(testResult[2:1000, 1], probs = c(0.025, 0.975)),
       quantile(testResult[2:1000, 2], probs = c(0.025, 0.975)),
       quantile(testResult[2:1000, 3], probs = c(0.025, 0.975)))
 
-# write.csv(trees, "trees.csv")
+# Saves dataset so we can use this for analysis in R 
+write.csv(trees, "trees.csv")
 
- 

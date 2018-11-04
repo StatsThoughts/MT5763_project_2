fe8147e40534dd12464f70a6cb89e9a3a6b7218b
Charisse <diamond_la@163.com>
2018-11-04 01:14:52 +0000

    change code and plot

--- code/Example analysis.R
+++ code/Example analysis.R
@@ -12,20 +12,20 @@ testResult <- lmBoot(trees, 1000, response = "Height")
 boxplot(testResult[2:1000, 1], col = "darkgreen")
 boxplot(testResult[2:1000, 2:3], col = "darkgreen")
 
-hist(testResult[,1], col = "slateblue4", main = 'intercept distribution')
-hist(testResult[,2], col = "slateblue4", main = 'Grith slope distribution')
-hist(testResult[,3], col = "slateblue4", main = 'Volume slope distribution')
+hist(testResult[2:1000, 1], col = "slateblue4", main = 'intercept distribution')
+hist(testResult[2:1000, 2], col = "slateblue4", main = 'Grith slope distribution')
+hist(testResult[2:1000, 3], col = "slateblue4", main = 'Volume slope distribution')
 
 # coef number for input data
 inputCoef <- testResult[1, ]
 
 # mean coef number for resampled data
-testMean <- c(mean(testResult[2:1000,1]), mean(testResult[2:1000,2]), mean(testResult[2:1000,3]))
+testMean <- c(mean(testResult[2:1000, 1]), mean(testResult[2:1000,2]), mean(testResult[2:1000,3]))
 
 # confidence interval by our bootstrap 
-rbind(quantile(testResult[2:1000,1], probs = c(0.025, 0.975)),
-      quantile(testResult[2:1000,2], probs = c(0.025, 0.975)),
-      quantile(testResult[2:1000,3], probs = c(0.025, 0.975)))
+rbind(quantile(testResult[2:1000, 1], probs = c(0.025, 0.975)),
+      quantile(testResult[2:1000, 2], probs = c(0.025, 0.975)),
+      quantile(testResult[2:1000, 3], probs = c(0.025, 0.975)))
 
 # write.csv(trees, "trees.csv")
 
